d825bb708fa47b88b9f2f231395c59d1b110a5fb
avangersKinkyClient <63201809+avangersKinkyClient@users.noreply.github.com>
2020-10-25 14:25:37 +0100

    create : public Cryption(string key_valeu, string iv_valeu)

--- CryptographyProject/Program.cs
+++ CryptographyProject/Program.cs
@@ -37,6 +37,30 @@ namespace CryptographyProject
             // password view
             private string password_string;
             private byte[] password_bytes;
+
+            // Constructor
+            public Cryption(string key_valeu, string iv_valeu)
+            {
+                Key = new byte[32];
+                IV = new byte[32];
+
+                int i;
+
+                mKey = key_valeu;
+                mIV = iv_valeu;
+
+                // key calculation, depends on first constructor parameter
+                for (i = 0; i < mKey.Length; i++)
+                {
+                    Key[i] = Convert.ToByte(mKey[i]);
+                }
+
+                // IV calculation, depends on second constructor parameter
+                for (i = 0; i < mIV.Length; i++)
+                {
+                    IV[i] = Convert.ToByte(mIV[i]);
+                }
+            }
         }
     }
 }
